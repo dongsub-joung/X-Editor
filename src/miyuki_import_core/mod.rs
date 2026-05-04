3607c8a584b72490914aeca17d5c5598b237f72b
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-04 18:52:23 +0900

    Add) enum ImportErrs - FailGetUserId

--- src/miyuki_import_core/mod.rs
+++ src/miyuki_import_core/mod.rs
@@ -1,17 +1,31 @@
 use crate::auth_x::*;
+use thiserror::Error;
 
 #[derive(Debug)]
 struct ImportXApi;
 
-impl ImportXApi{
-    async fn import_x_data(mut user_session: x_api_rs::TwAPI, primitive_type: Miyuki){
-        if !user_session.is_logged_in().await? { // @TODO if is_logged_in() consum lifetime, add &
-            user_session= auth_x::create_sesstion(user_session, primitive_type);
+#[derive(Debug)]
+struct FailGetUserId;
 
+#[derive(Error, Debug)]
+enum ImportErrs{
+    #[error("[FAILED] Geting user id")]
+    FailGetUserId(),
+}
+
+impl ImportXApi{
+    async fn import_x_data(mut user_session: x_api_rs::TwAPI, primitive_type: Miyuki) ->Result<Ok(), ImportErrs>{
+        if !user_session.is_logged_in().await? {
+            user_session= auth_x::create_sesstion(user_session, primitive_type);
         }
 
-        let user_id = api.me_rest_id().await?;
-        let res = api.get_followingids(user_id.to_string(), -1).await?;
+        let user_id = match user_session.me_rest_id().await{
+            Ok(res) => res,
+            Err(e) => {
+                return e;
+            },
+        };
+        let res = user_session.get_followingids(user_id.to_string(), -1).await?;
         tracing::debug!("response: {res:?}");
 
         let ids = res
@@ -20,7 +34,7 @@ impl ImportXApi{
             .map(|v| v.as_i64().unwrap_or_default().to_string())
             .collect();
 
-        let res = api.users_lookup(ids).await?;
+        let res = user_session.users_lookup(ids).await?;
         tracing::debug!("response: {res:?}");
 
         // loop {
