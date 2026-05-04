fc87a282f3301aee93bc76af476af82e220251d3
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-04 14:02:00 +0900

    Modify) fn verify_x_api

--- src/miyuki_core/mod.rs
+++ src/miyuki_core/mod.rs
@@ -2,6 +2,7 @@
 pub struct Auth;
 
 impl Auth{
+//     @TODO
     pub fn input_user_id(_str: String) -> String {
         _str.to_string()
     }
--- src/miyuki_import_core/mod.rs
+++ src/miyuki_import_core/mod.rs
@@ -4,10 +4,7 @@ use crate::auth_x;
 struct ImportXApi;
 
 impl ImportXApi{
-    async fn verify_x_api(){
-        // let cookies_path = PathBuf::from("cookies.json");
-        // let mut user_session = x_api_rs::TwAPI::new(Some(cookies_path.clone()))?;
-
+    async fn verify_x_api(mut user_session: x_api_rs::TwAPI){
         if !user_session.is_logged_in().await? {
             user_session= auth_x::create_sesstion();
         }
