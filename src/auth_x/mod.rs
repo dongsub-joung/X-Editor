83e5bd02124ebe6bfbfbf7e0f7d9ad1c59ad57a5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-04 14:33:49 +0900

    Extract & Add) async fn import_x_posts

--- src/auth_x/mod.rs
+++ src/auth_x/mod.rs
@@ -8,7 +8,7 @@ use crate::miyuki_core;
 pub struct XApi;
 
 impl XApi{
-    pub async fn create_sesstion() -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn create_sesstion(mut user_session: x_api_rs::TwAPI, primitive_type_auth: Miyuki) -> Result<(), Box<dyn std::error::Error>> {
 //         @TODO
         tracing_subscriber::fmt::init();
         let _ = dotenv();
@@ -23,37 +23,17 @@ impl XApi{
             if let Err(error) = result {
                 let error = error.downcast_ref::<SuspiciousLoginError>().unwrap();
                 println!("Enter your username (eg. @user): ");
-                let username= miyuki_core::Auth::input_user_id("");  // @TODO
+                let username= miyuki_core::Auth::input_user_id(primitive_type_auth);
                 api.login(&username, &password, "", Some(error.1.clone()))
                 .await?;
             }
             api.save_session().unwrap();
         }
+
         // always call this for extract csrf
         let is_logged_in = api.is_logged_in().await?;
         tracing::info!("is logged: {is_logged_in}");
 
-        /*
-         * let user_id = api.me_rest_id().await?;
-         * _let res = api.get_followingids(user_id.to_string(), -1).await?;
-         *    tracing::debug!("response: {res:?}");
-         *    let ids = res
-         *    .entries
-         *    .iter()
-         *    .map(|v| v.as_i64().unwrap_or_default().to_string())
-         *    .collect();
-         *    let res = api.users_lookup(ids).await?;
-         *    tracing::debug!("response: {res:?}");
-        */
-
-        // loop {
-        //     let pagination = api.get_friends(user_id, true, Some(cursor.into()))?;
-        //     cursor = pagination.cursor.clone();
-        //     tracing::debug!("Found {:?} following", pagination.entries.len());
-        //     if !pagination.has_more {
-        //         break;
-        //     }
-        // }
         Ok(())
     }
 }
--- src/miyuki_import_core/mod.rs
+++ src/miyuki_import_core/mod.rs
@@ -4,9 +4,31 @@ use crate::auth_x;
 struct ImportXApi;
 
 impl ImportXApi{
-    async fn import_x_posts(mut user_session: x_api_rs::TwAPI){
-        if !user_session.is_logged_in().await? {
-            user_session= auth_x::create_sesstion();
+    async fn import_x_posts(mut user_session: x_api_rs::TwAPI, primitive_type: Miyuki){
+        if !user_session.is_logged_in().await? { // @TODO if is_logged_in() consum lifetime, add &
+            user_session= auth_x::create_sesstion(user_session, primitive_type);
         }
+
+        /*
+         * let user_id = api.me_rest_id().await?;
+         * _let res = api.get_followingids(user_id.to_string(), -1).await?;
+         *    tracing::debug!("response: {res:?}");
+         *    let ids = res
+         *    .entries
+         *    .iter()
+         *    .map(|v| v.as_i64().unwrap_or_default().to_string())
+         *    .collect();
+         *    let res = api.users_lookup(ids).await?;
+         *    tracing::debug!("response: {res:?}");
+         */
+
+        // loop {
+        //     let pagination = api.get_friends(user_id, true, Some(cursor.into()))?;
+        //     cursor = pagination.cursor.clone();
+        //     tracing::debug!("Found {:?} following", pagination.entries.len());
+        //     if !pagination.has_more {
+        //         break;
+        //     }
+        // }
     }
 }
