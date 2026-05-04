7ca397a37b1a1d911fe9b0741b95f85dff59d036
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-04 14:25:17 +0900

    Rename) fn import_x_posts

--- src/miyuki_import_core/mod.rs
+++ src/miyuki_import_core/mod.rs
@@ -4,7 +4,7 @@ use crate::auth_x;
 struct ImportXApi;
 
 impl ImportXApi{
-    async fn verify_x_api(mut user_session: x_api_rs::TwAPI){
+    async fn import_x_posts(mut user_session: x_api_rs::TwAPI){
         if !user_session.is_logged_in().await? {
             user_session= auth_x::create_sesstion();
         }
