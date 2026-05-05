84c6003db774f88d59370b29d021a09cd7583cf1
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-06 05:39:08 +0900

    Add) Time & UpTime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod miyuki_core;
 mod miyuki_import_core;
 mod auth_x;
+mod utilities;
 
 fn main() {
     let miyuki= Rc<RefCell<miyuki_core::MiyukiGui::init()>>;
---
+++ src/utilities/mod.rs
@@ -0,0 +1,38 @@
+struct UpTime{
+    date: Date, // @TODO if date no needed for calculating Time, delete it
+    up_time: Time,
+}
+
+impl UpTime{
+    pub fn new() -> Self{
+        let data= date::Date::new();
+        let up_time= time::Time::new();  // @TODO find a crate for Time
+
+        self { data, up_time }
+    }
+    
+    pub fn get_date(&self) -> Time {
+        date::Date::get_now().fromat("%yyy%-%mm%-%dd%");
+    }
+
+    pub fn get_up_time(&self) -> &Self{
+        // @TODO calculate Time
+
+        self.up_time
+    }
+}
+
+struct DocumentaryReport{
+    user_id: UID,
+    up_time: UpTime,
+    
+}
+
+enum utilities{
+
+}
+
+
+impl utilities{
+
+}
