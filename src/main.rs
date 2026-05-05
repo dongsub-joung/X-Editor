23eead134896a0e8adc6263c70423d4c2125694e
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-05 17:46:01 +0900

    Add) main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@ mod miyuki_import_core;
 mod auth_x;
 
 fn main() {
-    println!("Hello, world!");
+    let miyuki= Rc<RefCell<miyuki_core::MiyukiGui::init()>>;
+
+    loop{
+        miyuki_core::capturing(&miyuki);
+    }
 }
