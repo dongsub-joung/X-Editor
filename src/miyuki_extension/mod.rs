f3b67b83a83325d0eca780a649a6f5985871631a
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-06 07:38:02 +0900

    Struct & impl clipboard

---
+++ src/miyuki_extension/mod.rs
@@ -0,0 +1,13 @@
+use clipboard_win::{Clipboard, formats, Getter, Setter};
+
+struct Clipboard{
+    v_clip_string: Vec<String>
+}
+
+impl Clipboard{
+    pub fn get_clipboard() -> Clipboard {
+        let v_clip_string= // clipboard_win::get_clipboard_string()
+
+        self { v_clip_string }
+    }
+}
