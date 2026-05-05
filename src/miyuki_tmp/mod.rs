82e7f1d0f616785bb075f7c20892e0d88b70cd66
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-06 07:21:40 +0900

    Add) save_snapshot

--- src/miyuki_tmp/mod.rs
+++ src/miyuki_tmp/mod.rs
@@ -1,11 +1,13 @@
-struct RemainedData{
-    user_data: SnapShot
+pub struct RemainedData{
+    pub user_data: SnapShot
 }
 
 impl RemainedData{
    pub save_snapshot() -> Result<Ok(), Box<dyn Error::IO>>{
        // @TODO create let snapshot
+       utilities::IO::write_file(&snapshot)?;
 
-       utilities::IO::write_file(snapshot)?;
+       RemainedData::user_data= snapshot;
+       Ok(_)
    }
 }
