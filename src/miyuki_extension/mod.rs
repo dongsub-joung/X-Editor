1558d0528c0530934d633e58dadf4addabe48af1
DongSub_Joung <ehdtjqwjd@kangwon.ac.kr>
2026-05-11 19:27:21 +0900

    Answer

--- src/miyuki_extension/mod.rs
+++ src/miyuki_extension/mod.rs
@@ -2,6 +2,7 @@ use clipboard_win::{Clipboard, formats, Getter, Setter};
 
 // @TODO If Miyuki extension use system call, Antivirus will block Miyuki
 // How can I do?
+// https://x.com/jds_invoker/status/2053783852347425270
 
 struct Clipboard{
     v_clip_string: Vec<String>
