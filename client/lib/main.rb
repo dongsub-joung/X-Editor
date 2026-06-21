e7eb328d321dfa456ccdda97085a2b8362d80813
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-22 04:40:36 +0900

    Add) exception throw

--- client/lib/main.rb
+++ client/lib/main.rb
@@ -1,24 +1,27 @@
 require 'libui'
 
 UI = LibUI
-
 UI.init
 
-main_window = UI.new_window('Miyuki X editor', 200, 700, 1)
-
+begin
+  main_window = UI.new_window('Miyuki X editor', 200, 700, 1)
+rescue 
+  catch(:init) do
+    throw :init if !main_window? 
+end
 # UI.new_text box('text_box')
 # (ranndering) UI.new_viwer('viwer')
-# btm(post)
-button = UI.new_button('Button')
+button = UI.new_button('Button') #post
 
 UI.button_on_clicked(button) do
   # do post request rust server
   UI.msg_box(main_window, 'Information', 'You clicked the button')
 end
 
+# options
 UI.window_set_child(main_window, button) # text box, vIwer box
-
 UI.control_show(main_window) #sub widow - viwer
 
+# end
 UI.main
 UI.quit
