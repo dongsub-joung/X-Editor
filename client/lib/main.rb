5ad3ecf38649e45a74def452a1951ecec0ad3873
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-22 04:26:03 +0900

    Refector) UI

--- client/lib/main.rb
+++ client/lib/main.rb
@@ -6,19 +6,19 @@ UI.init
 
 main_window = UI.new_window('Miyuki X editor', 200, 700, 1)
 
-# text box
-# ranndering Viwer
-
+# UI.new_text box('text_box')
+# (ranndering) UI.new_viwer('viwer')
 # btm(post)
 button = UI.new_button('Button')
 
-# do post request rust server
 UI.button_on_clicked(button) do
+  # do post request rust server
   UI.msg_box(main_window, 'Information', 'You clicked the button')
 end
 
 UI.window_set_child(main_window, button) # text box, vIwer box
-UI.control_show(main_window) #sub widows(viwer)
+
+UI.control_show(main_window) #sub widow - viwer
 
 UI.main
 UI.quit
