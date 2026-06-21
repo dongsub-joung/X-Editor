8ca20764e612fdbbeb925c2f02e99f55e5499303
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-22 04:08:58 +0900

    Unit Test) user logic

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,32 @@ mod miyuki_user;
 
 #[cfg(test)]
 mod tests {
+    use crate::miyuki_user::Language;
     use super::*;
+    use compile_type_eq::{assert_types_eq, assert_types_not_eq};
 
     #[test]
     fn it_works() {
-        let user= miyuki_user::User::new(LanguageOptions::JAPANESE);
-        let user_uid= user.get_user_data();
-
-        assert_eq!(user_uid, user.uid);
+        let user= miyuki_user::User::new(
+            miyuki_user::Language::new(
+                miyuki_user::LanguageOptions::JAPANESE
+            )
+        );
+        let user_uid= user.get_user_uid();
+        
+        assert_eq!(user_uid, &user.uid);
     }
-
+    
     #[test]
     fn language(){
-        let user= User::new(LanguageOptions::JAPANESE);
+        let user= miyuki_user::User::new(
+            Language::new(miyuki_user::LanguageOptions::JAPANESE)
+        );
+        let lang_option= Language::new(
+            miyuki_user::LanguageOptions::JAPANESE
+        );
 
-        assert_eq!(LanguageOptions::JAPANESE, user.language);
+        // @TODO type compare
+        // assert_types_eq::<i32, i32>();
     }
 }
