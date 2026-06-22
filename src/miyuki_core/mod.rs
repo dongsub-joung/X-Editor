b0d3aeae92dac691b9d7d583cfd6a31e94166f4e
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-23 05:46:06 +0900

    Fix) trait Regular

--- src/miyuki_core/mod.rs
+++ src/miyuki_core/mod.rs
@@ -1,5 +1,83 @@
 use thiserror::Error;
 use suffix::SuffixTable;
+use dotenvy::dotenv;
+use std::path::PathBuf;
+use x_api_rs::auth::SuspiciousLoginError;
+
+
+struct Miyuki{
+
+}
+
+trait Regular{
+    fn from_str(string: String) -> Self;
+}
+
+struct ImporingXPost{
+    x_url: String,
+}
+
+impl Regular for ImporingXPost{
+    fn from_str(string: String) -> Self{
+        ImporingXPost { x_url: string }
+    }
+}
+
+#[derive(Debug)]
+pub struct Auth;
+
+impl Auth{
+    pub fn input_user_id(user_inpute: String) -> String {
+        let mut checked= Regular::check(user_inpute); 
+        let if !Some(_string) = checked{
+            checked= Default::default();
+        }
+
+        checked
+    }
+}
+
+#[derive(Debug)]
+struct XApi;
+
+pub impl XApi{
+    pub async fn create_sesstion(mut user_session: x_api_rs::TwAPI, primitive_type_auth: Miyuki) -> Result<(), Box<dyn std::error::Error>> {
+        tracing_subscriber::fmt::init();
+        let _ = dotenv();
+        let cookies_path = PathBuf::from("cookies.json");
+        let username = std::env::var("USERNAME")?;
+        let password = std::env::var("PASSWORD")?;
+        tracing::debug!("username: {username}");
+        tracing::debug!("password: {password}");
+
+        let mut api = x_api_rs::TwAPI::new(Some(cookies_path.clone()))?;
+        if !cookies_path.exists() {
+            let result = api.login(&username, &password, "", None).await;
+            if let Err(error) = result {
+                let login_err: &SuspiciousLoginError;
+                if let Some(error) = error.downcast_ref::<SuspiciousLoginError>(){
+                    login_err= error;
+                };
+                let username= uti::input_user_id(primitive_type_auth);
+                if let Ok(None)= api.login(&username, &password, "", Some(error.1.clone()))
+                .await{
+                    eprintln!("{:?}", login_err);
+                };
+            }
+
+            match api.save_session(){
+                Err(e) => { eprintln!("{}", e); }
+                _ => { dbg!("[OK] Session connected"); },
+            };
+        }
+
+        // always call this for extract csrf
+        let is_logged_in = api.is_logged_in().await?;
+        tracing::info!("is logged: {is_logged_in}");
+
+        Ok(())
+    }
+}
 
 
 #[derive(Debug)]
@@ -31,9 +109,7 @@ pub enum ImportErrs{
 }
 
 impl ImportXApi{
-    pub async fn import_x_data<F>(mut user_session: F, primitive_type: miyuki)
-    where
-        F: Future<Output= x_api_rs::TwAPI>
+    pub async fn import_x_data<F>(mut user_session: dyn Future<Output= x_api_rs::TwAPI>, primitive_type: miyuki_core::OneLine)
     // -> Result<Box<dyn ImportErrs>
     {
         if !user_session.is_logged_in().await{
@@ -109,20 +185,6 @@ impl ImportXApi{
 
 // @TODO Miyiki_core is GUI thread
 
-// @TODO Auth used on other btn option body
-#[derive(Debug)]
-pub struct Auth;
-
-impl Auth{
-    pub fn input_user_id(user_inpute: String) -> String {
-        let mut checked= utilities::Utilities::Regular::check(user_inpute); 
-        let if !Some(_string) = checked{
-            checked= Default::default();
-        }
-
-        checked
-    }
-}
 
 struct OneLine{
     line: HashMap<Miyuki_GUI_Componante, String>;
@@ -187,4 +249,3 @@ impl Caputre{
         thread::exit(0);
     }
 }
-
